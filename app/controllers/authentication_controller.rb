f5757a3baa6e579d5d1af59c4673e871002a13af
BahirHakimy <bahirhakimy2020@gmail.com>
2023-07-25 15:29:55 +0430

    Added jwt

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,23 @@
+class AuthenticationController < ApplicationController
+  protect_from_forgery with: :null_session
+  before_action :authorize_request, except: :login
+
+  # POST /auth/login
+  def login
+    @user = User.where(username: params[:username]).first
+    if @user&.authenticate(params[:password])
+      token = JsonWebToken.encode(user_id: @user.id)
+      time = Time.now + 24.hours.to_i
+      render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
+                     username: @user.username }, status: :ok
+    else
+      render json: { error: 'unauthorized' }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def login_params
+    params.permit(:username, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+# config/routes.rb
   # Defines the root path route ("/")
+  resources :users, param: :_username
+  post '/auth/login', to: 'authentication#login'
+  
   root 'root#index'
-
+  
   get '*path', to: 'root#index'
 end
