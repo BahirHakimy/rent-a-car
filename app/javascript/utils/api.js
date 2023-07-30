ba5e96f22aea9b2775b4198b050348c1ef9562b1
BahirHakimy <bahirhakimy2020@gmail.com>
2023-07-30 10:55:45 +0430

    Added auto logout

--- app/javascript/utils/api.js
+++ app/javascript/utils/api.js
@@ -1,13 +1,25 @@
 import axios from 'axios';
-import { clearSession, getUser } from './auth';
+import { clearSession } from './auth';
 
-const authFetch = (token) =>
-  axios.create({
+const authFetch = (token) => {
+  const instance = axios.create({
     headers: {
       Authorization: `Bearer ${token}`,
       'Content-Type': 'application/json',
       Accept: 'application/json',
     },
   });
+  instance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (error.response.status === 401) {
+        clearSession();
+        window.location.reload();
+      }
+      return Promise.reject(error);
+    }
+  );
+  return instance;
+};
 
 export { authFetch as axios };
